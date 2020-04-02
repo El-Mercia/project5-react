459d252c0a17d3173b02622caac8cfb2c164d3ad
Mercia <merciarandrianome@gmail.com>
2020-04-02 16:13:48 +0100

    add nav & link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
 
 import {
     BrowserRouter as Router,
     Switch,
     Route,
+    Link,
 } from 'react-router-dom';
 import Home          from './pages/Home';
 import CreateArticle from './pages/CreateArticle';
@@ -14,6 +16,23 @@ import NotFound      from './pages/NotFound';
 const App = () => {
   return (
     <Router>
+      <Nav>
+        <Nav.Item>
+          <Nav.Link as={Link}  to="/">
+            Home
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/articles/create">
+            Create article
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/articles/delete">
+            Delete article
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
       <Switch>
         <Route exact path="/"          component={Home} />
         <Route path="/articles/create" component={CreateArticle}  />
