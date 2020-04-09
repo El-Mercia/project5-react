1dd706063a485648c1f386212c33c7d96fcbea18
Mercia <merciarandrianome@gmail.com>
2020-04-09 09:43:09 +0100

    add fetch in DeleteComment.js

--- src/pages/DeleteComment.js
+++ src/pages/DeleteComment.js
@@ -1,5 +1,5 @@
 import React, { useState} from 'react';
-
+import { toast } from 'react-toastify';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -11,8 +11,37 @@ const DeleteComment = () => {
     const handleSubmit = (event ) => {
         event.preventDefault();
 
-        console.log("id : ", id);
-    }
+        fetch('http://localhost:3001/api/comments/delete', {
+            method : "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify ({
+                id,
+            }),
+        })
+        .then((result) => {
+            return result.json();
+        })
+        .then(({ status, extra }) => {
+           if (status === "OK") {
+                setId("");
+                toast.success("Comments has been successfully added");   
+           } else {
+               toast.error(
+                <div>
+                    Oups... error <br />
+                    {extra}
+                </div>
+               );
+           }
+        
+        })
+        .catch((error) => {
+            toast.error("Oups... error");
+        });
+        };
 
     const  handleChange = (event) => {
         switch (event.target.name) {
